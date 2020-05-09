645eb9d17389dee0126fb329a9631276b4b5f73d
M6code <benjamynfrank101@gmail.com>
2020-05-09 12:23:10 +0100

    Make request to backend DRF

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,66 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+export class App extends React.Component {
 
   constructor(props) {
     super(props)
-
+  
     this.state = {
-      
+       todoList: [],
+       activeItem: {
+         id: null,
+         title: "",
+         completed: false,
+       },
+       editing: false,
     }
+    this.fetchTasks = this.fetchTasks.bind(this)
   }
 
+  componentDidMount(){
+    this.fetchTasks()
+  }
 
-  return (
-    <div className="container">
+  fetchTasks(){
+    console.log('fetching........')
+    fetch("http://127.0.0.1:8000/api/task-list/")
+    .then(response => response.json())
+    .then(data => 
+      console.log("Data: ", data)
+      )
+  }
+  
 
-      <div className="task-container">
-        <div id="form-wrapper">
-          <form id="form">
-            <div className="flex-wrapper">
-              <div style={{ flex: 6 }}>
-                <input className="from-control" id="title" type="text" name="title" placeholder="Enter a task" />
+
+  render() {
+    return (
+      <div className="container">
+
+        <div className="task-container">
+          <div id="form-wrapper">
+            <form id="form">
+              <div className="flex-wrapper">
+                <div style={{ flex: 6 }}>
+                  <input className="from-control" id="title" type="text" name="title" placeholder="Enter a task" />
+                </div>
+
+                <div style={{ flex: 1 }}>
+                  <input id="submit" className="btn btn-warning" type="submit" name="Add" />
+                </div>
+
               </div>
+            </form>
+          </div>
 
-              <div style={{ flex: 1 }}>
-                <input id="submit" className="btn btn-warning" type="submit" name="Add" />
-              </div>
+          <div id="list-wrapper">
 
-            </div>
-          </form>
-        </div>
-
-        <div id="list-wrapper">
-
+          </div>
         </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
+
